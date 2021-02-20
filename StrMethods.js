f8618942437de3eb2d6cd7b4482702ca9fd1bf52
Divya9515 <50729779+Divya9515@users.noreply.github.com>
2021-02-20 07:02:00 +0530

    Add files via upload
    String operations slice and split are shown.

---
+++ StrMethods.js
@@ -0,0 +1,8 @@
+
+  var str="This is a String";
+  console.log(str);
+  var slres=str.slice(0,3);
+  console.log("slres : "+slres);
+  var res=str.split(" ");
+  console.log("Result after splitting the string is "+res);
+
